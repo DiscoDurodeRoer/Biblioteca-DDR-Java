7ee6c2cdc1881ccec8e30ea9788c5ec3662d2fb3
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2018-07-26 21:40:17 +0200

    Eliminado ConexionDB, movido a otro proyecto.

--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/clases/bd/ConexionODBC.java
+++
@@ -1,9 +0,0 @@
-package es.discoduroderoer.clases.bd;
-
-public class ConexionODBC extends ConexionDB {
-
-    public ConexionODBC() {
-        super("sun.jdbc.odbc.JdbcOdbcDriver", "jdbc:odbc:MYSQL_ODBC");
-    }
-
-}
--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/clases/bd/ConexionSQLite.java
+++
@@ -1,8 +0,0 @@
-package es.discoduroderoer.clases.bd;
-
-public class ConexionSQLite extends ConexionDB {
-
-    public ConexionSQLite(String ruta) {
-        super("org.sqlite.JDBC", "jdbc:sqlite:" + ruta);
-    }
-}
