282e55f55012bf6bcad4c487c2339f797263dd5f
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2018-08-05 21:42:27 +0200

    Añadida la función numeroHoras

---
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/fechas/Horas.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.discoduroderoer.fechas;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Horas {
+
+    /**
+     * devuelve la diferencia entre dos horas
+     * solo horas en punto y media hora (08:00, 08:30, 09:00)
+     * @param horaInicio
+     * @param horaFin
+     * @return 
+     */
+    public static double numeroHoras(String horaInicio, String horaFin) {
+
+        double numHoraInicio;
+        double numHoraFin;
+
+        String[] horaMinutoInicio = horaInicio.split(":");
+
+        numHoraInicio = Double.parseDouble(horaMinutoInicio[0]);
+        if (Integer.parseInt(horaMinutoInicio[1]) == 30) {
+            numHoraInicio += 0.5;
+        }
+
+        String[] horaMinutoFin = horaFin.split(":");
+
+        numHoraFin = Double.parseDouble(horaMinutoFin[0]);
+        if (Integer.parseInt(horaMinutoFin[1]) == 30) {
+            numHoraFin += 0.5;
+        }
+
+        return numHoraFin - numHoraInicio;
+
+    }
+}
