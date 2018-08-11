8f642ca93ef5b012986d7fd8d6ff0eb8e229a0b9
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2018-08-11 19:26:39 +0200

    Añadida funcion limpiarFormulario

--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/swing/MiSwing.java
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/swing/MiSwing.java
@@ -17,18 +17,26 @@ import java.net.URISyntaxException;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.InputMap;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDesktopPane;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
 import javax.swing.JViewport;
 import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
@@ -361,6 +369,47 @@ public class MiSwing {
             }
         }
         return null;
+
+    }
+    
+    /**
+     * Llamalo inicialemente con this.getContentPane().getComponents()
+     *
+     * @param componentes
+     */
+    public static void limpiarFormulario(Component[] componentes) {
+
+        for (int i = 0; i < componentes.length; i++) {
+            if (componentes[i] instanceof JPanel) {
+                limpiarFormulario(((JPanel) componentes[i]).getComponents());
+            } else if (componentes[i] instanceof JViewport) {
+                limpiarFormulario(((JViewport) componentes[i]).getComponents());
+            } else if (componentes[i] instanceof JScrollPane) {
+                limpiarFormulario(((JScrollPane) componentes[i]).getComponents());
+            } else if (componentes[i] instanceof JTextField) {
+                ((JTextField) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JTextArea) {
+                ((JTextArea) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JPasswordField) {
+                ((JPasswordField) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JCheckBox) {
+                ((JCheckBox) componentes[i]).setSelected(false);
+            } else if (componentes[i] instanceof JRadioButton) {
+                ((JRadioButton) componentes[i]).setSelected(false);
+            } else if (componentes[i] instanceof JToggleButton) {
+                ((JToggleButton) componentes[i]).setSelected(false);
+            } else if (componentes[i] instanceof JComboBox) {
+                ((JComboBox) componentes[i]).setSelectedIndex(0);
+            } else if (componentes[i] instanceof JSlider) {
+                ((JSlider) componentes[i]).setValue(((JSlider) componentes[i]).getMinimum());
+            } else if (componentes[i] instanceof JSpinner) {
+                ((JSpinner) componentes[i]).setValue(0);
+            } else if (componentes[i] instanceof JFormattedTextField) {
+                ((JFormattedTextField) componentes[i]).setText("");
+            } else if (componentes[i] instanceof JTable) {
+                ((DefaultTableModel) (((JTable) componentes[i]).getModel())).setRowCount(0);
+            }
+        }
 
     }
 
