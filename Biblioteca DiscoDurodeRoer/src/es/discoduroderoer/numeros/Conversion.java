6b3e92a0c871a553f3a4176facfa59f563444a0e
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2020-08-21 18:55:10 +0200

    BasexBaseY mejorado

--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/numeros/Conversion.java
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/numeros/Conversion.java
@@ -177,7 +177,6 @@ public class Conversion {
 
     }
 
-    
     /**
      * Convierte un numero de una base a otra
      *
@@ -187,24 +186,26 @@ public class Conversion {
      * @return numero en formato decimal
      */
     public static int basexABaseY(long numero, int basex, int basey) {
-
-        if (basex == 10 || basey == 10) {
-            int decimal = 0;
-            int digito;
-            final long DIVISOR = basey;
-            for (long i = numero, j = 0; i > 0; i /= DIVISOR, j++) {
-                digito = (int) (i % DIVISOR);
-                decimal += digito * Math.pow(basex, j);
+        if (basex >= 2 && basex <= 10 && basey >= 2 && basey <= 10) {
+            if (basex == 10 || basey == 10) {
+                int decimal = 0;
+                int digito;
+                final long DIVISOR = basey;
+                for (long i = numero, j = 0; i > 0; i /= DIVISOR, j++) {
+                    digito = (int) (i % DIVISOR);
+                    decimal += digito * Math.pow(basex, j);
+                }
+                return decimal;
+            } else {
+                // Lo paso a decimal
+                int aDecimal = basexABaseY(numero, basex, 10);
+                // Lo paso a la base deseada
+                int numeroBaseY = basexABaseY(aDecimal, 10, basey);
+                return numeroBaseY;
             }
-            return decimal;
         } else {
-            // Lo paso a decimal
-            int aDecimal = basexABaseY(numero, basex, 10);
-            // Lo paso a la base deseada
-            int numeroBaseY = basexABaseY(aDecimal, 10, basey);
-            return numeroBaseY;
+            return -1;
         }
-
     }
 
 }
