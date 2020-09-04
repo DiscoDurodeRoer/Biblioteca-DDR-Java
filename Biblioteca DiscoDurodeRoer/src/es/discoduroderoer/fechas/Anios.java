1698c4fe1c8b2fbc02eb96ecda026203e14e3e08
Gregorio Martínez Mejías <67093363+6regorio@users.noreply.github.com>
2020-09-04 20:12:50 +0200

    Update Anios.java
    Date cambiado a LocalDate por dar @deprecated

--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/fechas/Anios.java
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/fechas/Anios.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.discoduroderoer.fechas;
 
+import java.time.LocalDate;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 
 /**
@@ -54,12 +49,12 @@ public class Anios {
     public static int calcularAnios(int dia, int mes, int anio) {
 
         //Fecha actual
-        Date actual = new Date();
+        LocalDate actual = LocalDate.now();
 
-        //Cojo los datos necesarios
-        int diaActual = actual.getDate();
-        int mesActual = actual.getMonth() + 1;
-        int anioActual = actual.getYear() + 1900;
+        //Cojo los datos necesarios        
+        int diaActual = actual.getDayOfMonth();
+        int mesActual = actual.getMonthValue()+ 1;
+        int anioActual = actual.getYear()+ 1900;
 
         //Diferencia de años
         int diferencia = anioActual - anio;
