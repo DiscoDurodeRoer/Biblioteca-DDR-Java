511d998089e007817f71c6d9f898953334107eba
Gregorio Martínez Mejías <67093363+6regorio@users.noreply.github.com>
2020-09-05 10:59:34 +0200

    Update to LocalDate

--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/fechas/Conversion.java
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/fechas/Conversion.java
@@ -1,12 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package es.discoduroderoer.fechas;
 
 import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import javax.swing.JOptionPane;
 
@@ -15,13 +13,45 @@ import javax.swing.JOptionPane;
  */
 public class Conversion {
     
+     /**
+     * Convierte una fecha a formato científico
+     * Por ejemplo: 05/02/1990 = 19900205
+     * @param fecha Fecha a convertir LocalDate
+     * @return Fecha en formato científico
+     */
+    public static int deFechaANumero(LocalDate fecha){
+        
+        String f = String.valueOf(fecha.getYear());
+        
+        String formato="MM";
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern(formato);        
+        
+        if (Integer.parseInt(dateFormat.format(fecha)) < 10) {
+            f+="0"+Integer.parseInt(dateFormat.format(fecha));
+        }else{
+            f+=Integer.parseInt(dateFormat.format(fecha));
+        }
+        
+        formato="dd";        
+        dateFormat = DateTimeFormatter.ofPattern(formato); 
+        
+        if (Integer.parseInt(dateFormat.format(fecha)) <10){
+            f+="0"+Integer.parseInt(dateFormat.format(fecha));
+        }else{
+            f+=Integer.parseInt(dateFormat.format(fecha));
+        }
+        
+        return Integer.parseInt(f);
+    }
+  
     /**
      * Convierte una fecha a formato científico
      * Por ejemplo: 05/02/1990 = 19900205
-     * @param fecha Fecha a convertir
+     * @param fecha Fecha a convertir Date
      * @return Fecha en formato científico
      */
-    public static int deFechaANumero(Date fecha){
+    @Deprecated
+    public static int deFechaDateANumero(Date fecha){
         
         String f = String.valueOf(fecha.getYear()+1900);
         
@@ -45,14 +75,41 @@ public class Conversion {
         
         return Integer.parseInt(f);
     }
+    
+    /**
+     * Pasa de número en formato científico a fecha
+     * Por ejemplo:  19900205 = 05/02/1990
+     * @param numero Número en formato científico
+     * @return Fecha LocalDate
+     */
+    public static LocalDate deNumeroAFecha(int numero){
+        
+        try{
+            
+            int anio=(int)Math.floor(numero/10000);
+            
+            int mes = (int)Math.floor(((numero%10000) / 100));
+            
+            int dia=numero%100;
+            
+            LocalDate fecha=LocalDate.of(anio, mes, dia);    //new Date(anio-1900, mes-1, dia);
+            
+            return fecha;
+            
+        }catch(DateTimeException e){
+            JOptionPane.showMessageDialog(null, "Error, la fecha no es correcta");
+            return null;
+        }
+    }
 
     /**
      * Pasa de número en formato científico a fecha
      * Por ejemplo:  19900205 = 05/02/1990
      * @param numero Número en formato científico
-     * @return Fecha
+     * @return Fecha Date
      */
-    public static Date deNumeroAFecha(int numero){
+    @Deprecated
+    public static Date deNumeroAFechaDate(int numero){
         
         try{
             
@@ -71,13 +128,47 @@ public class Conversion {
             return null;
         }
     }
+        
+    /**
+     * Convierte una hora a número en formato científico
+     * Por ejemplo:  20:50:40 = 205040
+     * @param hora Hora pasada (LocalTime)
+     * @return Hora en formato científico
+     */
+    public static int deHoraANumero(LocalTime hora){        
+        
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm:ss"); 
+        
+        String f;
+        
+        if (hora.getHour()<10){
+            f="0"+hora.getHour();
+        }else{
+            f=String.valueOf(hora.getHour());
+        }
+        
+        if(hora.getMinute()< 10){
+            f+="0"+hora.getMinute();
+        }else{
+            f+=hora.getMinute();
+        }
+        
+        if(hora.getSecond()<10){
+            f+="0"+hora.getSecond();
+        }else{
+            f+=hora.getSecond();
+        }
+        
+        return Integer.parseInt(f);
+    }
     
     /**
      * Convierte una hora a número en formato científico
      * Por ejemplo:  20:50:40 = 205040
-     * @param hora Hora pasada
+     * @param hora Hora pasada (Date)
      * @return Hora en formato científico
      */
+    @Deprecated
     public static int deHoraANumero(Date hora){
         
         SimpleDateFormat formato=new SimpleDateFormat("HH:mm:ss");
@@ -104,14 +195,34 @@ public class Conversion {
         
         return Integer.parseInt(f);
     }
+        
+    /**
+     * Convierte un número en formato científico a hora
+     * Por ejemplo: 205040 = 20:50:40
+     * @param numero Número es científico
+     * @return Hora LocalTime
+     */
+    public static LocalTime deNumeroAHora(int numero){
+        
+        int hora=(int)Math.floor(numero/10000);
+        
+        int minuto= (int)Math.floor((numero%10000)/100);
+        
+        int segundo=numero%100;
+        
+        LocalTime fecha=LocalTime.of(hora, minuto, segundo);
+        
+        return fecha;        
+    }
     
     /**
      * Convierte un número en formato científico a hora
      * Por ejemplo: 205040 = 20:50:40
      * @param numero Número es científico
-     * @return Hora
+     * @return Hora Date
      */
-    public static Date deNumeroAHora(int numero){
+    @Deprecated
+    public static Date deNumeroAHoraDate(int numero){
         
         int hora=(int)Math.floor(numero/10000);
         
