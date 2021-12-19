d3ee9eea0e725a6c48a2a7be674cb904ae01932b
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2021-12-19 14:35:33 +0100

    - adyancentesA - Modificada las conversiones de años

--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/matrices/Matrices.java
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/matrices/Matrices.java
@@ -1,64 +1,91 @@
 package es.discoduroderoer.matrices;
+
 import es.discoduroderoer.arrays.Array;
+
 /**
  * @author DiscoDurodeRoer
  */
 public class Matrices {
-    
+
     /**
      * Devuelve las posiciones adyacentes de una posicion en una matriz de int
+     *
      * @param matriz Matriz
      * @param fila Fila
      * @param columna Columna
      * @return Posiciones adyacentes
      */
-    public static int[] adyacentesA(int matriz[][], int fila, int columna){
-        
-        int tamanio=matriz.length;
-        
-        int dirs[] = {1, -1, 1, 0, 1, 1, 0, 1, -1, 1, -1, 0, -1, -1, 0, -1};
-        
-        int posicionesValidas[]=new int[17];
+    public static int[] adyacentesA(int matriz[][], int fila, int columna) {
 
-        int indice2=0;
-        for(int indice=0;indice<dirs.length;indice+=2){
-           if(fila + dirs[indice] >= 0 && fila + dirs[indice] < tamanio && columna + dirs[indice + 1] >= 0 && columna + dirs[indice + 1] < tamanio){
-              posicionesValidas[indice2] = fila + dirs[indice];
-              posicionesValidas[indice2+1] = columna + dirs[indice + 1];
-              indice2 += 2;
-           } 
+        int tamanio = matriz.length;
+
+        int dirs[] = {1, -1, 1, 0, 1, 1, 0, 1, -1, 1, -1, 0, -1, -1, 0, -1};
+
+        int posicionesValidas[] = new int[17];
+
+        int indice2 = 0;
+        for (int indice = 0; indice < dirs.length; indice += 2) {
+            if (fila + dirs[indice] >= 0 && fila + dirs[indice] < tamanio && columna + dirs[indice + 1] >= 0 && columna + dirs[indice + 1] < tamanio) {
+                posicionesValidas[indice2] = fila + dirs[indice];
+                posicionesValidas[indice2 + 1] = columna + dirs[indice + 1];
+                indice2 += 2;
+            }
         }
-        
+
         return Array.redimensionar(posicionesValidas, indice2);
     }
-    
+
     /**
-     * Devuelve las posiciones adyacentes de una posicion en una matriz de Objetos
-     * @param matriz Matriz
-     * @param fila Fila
-     * @param columna Columna
-     * @return Posiciones adyacentes
+     * Devuelve las posiciones adyacentes de una posicion concreta
+     *
+     * @param filas que tiene la matriz
+     * @param columnas que tiene la matriz
+     * @param filaOrigen de donde queremos sacar las posiciones adyacentes
+     * @param columnaOrigen de donde queremos sacar las posiciones adyacentes
+     * @return Matriz con las posiciones adyacentes
      */
-    public static int[] adyacentesA(Object matriz[][], int fila, int columna){
-        
-        int tamanio=matriz.length;
-        
-        int dirs[] = {1, -1, 1, 0, 1, 1, 0, 1, -1, 1, -1, 0, -1, -1, 0, -1};
-        
-        int posicionesValidas[]=new int[17];
+    public static int[][] adyacentesA(int filas, int columnas, int filaOrigen, int columnaOrigen) {
 
-        int indice2=0;
-        for(int indice=0;indice<dirs.length;indice+=2){
-           if(fila + dirs[indice] >= 0 && fila + dirs[indice] < tamanio && columna + dirs[indice + 1] >= 0 && columna + dirs[indice + 1] < tamanio){
-              posicionesValidas[indice2] = fila + dirs[indice];
-              posicionesValidas[indice2+1] = columna + dirs[indice + 1];
-              indice2 += 2;
-           } 
+        if (!(filaOrigen >= 0
+                && filaOrigen <= (filas - 1)
+                && columnaOrigen >= 0
+                && columnaOrigen <= (columnas - 1))) {
+            return null;
+        }
+
+        int numPosiciones;
+
+        if (filaOrigen == 0 || filaOrigen == (filas - 1)) {
+            if (columnaOrigen == 0 || columnaOrigen == (columnas - 1)) {
+                numPosiciones = 3;
+            } else {
+                numPosiciones = 5;
+            }
+        } else {
+            if (columnaOrigen == 0 || columnaOrigen == (columnas - 1)) {
+                numPosiciones = 5;
+            } else {
+                numPosiciones = 8;
+            }
         }
         
-        return Array.redimensionar(posicionesValidas, indice2);
+        int[][] posiciones = new int[numPosiciones][2];
+        
+        int indicePosicion = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+
+                if ((i != 0 || j != 0)
+                        && (filaOrigen + i) >= 0
+                        && (filaOrigen + i) <= (filas - 1)
+                        && (columnaOrigen + j) >= 0
+                        && (columnaOrigen + j) <= (columnas - 1)) {
+                    posiciones[indicePosicion][0] = filaOrigen + i;
+                    posiciones[indicePosicion][1] = columnaOrigen + j;
+                    indicePosicion++;
+                }
+            }
+        }
+        return posiciones;
     }
-    
-    
-    
 }
--- Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/numeros/Conversion.java
+++ Biblioteca DiscoDurodeRoer/src/es/discoduroderoer/numeros/Conversion.java
@@ -185,27 +185,27 @@ public class Conversion {
      * @param basey Base a la que queremos convertir
      * @return numero en formato decimal
      */
-    public static int basexABaseY(long numero, int basex, int basey) {
-        if (basex >= 2 && basex <= 10 && basey >= 2 && basey <= 10) {
-            if (basex == 10 || basey == 10) {
-                int decimal = 0;
-                int digito;
-                final long DIVISOR = basey;
-                for (long i = numero, j = 0; i > 0; i /= DIVISOR, j++) {
-                    digito = (int) (i % DIVISOR);
-                    decimal += digito * Math.pow(basex, j);
-                }
-                return decimal;
-            } else {
-                // Lo paso a decimal
-                int aDecimal = basexABaseY(numero, basex, 10);
-                // Lo paso a la base deseada
-                int numeroBaseY = basexABaseY(aDecimal, 10, basey);
-                return numeroBaseY;
+public static int basexABaseY(long numero, int basex, int basey) {
+    if (basex >= 2 && basex <= 10 && basey >= 2 && basey <= 10) {
+        if (basex == 10 || basey == 10) {
+            int decimal = 0;
+            int digito;
+            final long DIVISOR = basey;
+            for (long i = numero, j = 0; i > 0; i /= DIVISOR, j++) {
+                digito = (int) (i % DIVISOR);
+                decimal += digito * Math.pow(basex, j);
             }
+            return decimal;
         } else {
-            return -1;
+            // Lo paso a decimal
+            int aDecimal = basexABaseY(numero, basex, 10);
+            // Lo paso a la base deseada
+            int numeroBaseY = basexABaseY(aDecimal, 10, basey);
+            return numeroBaseY;
         }
+    } else {
+        return -1;
     }
+}
 
 }
